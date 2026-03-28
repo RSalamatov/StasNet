ea66d6490de840c2639a7786bd01beb49148ad43
RSalamatov <salamatov.roman7030@gmail.com>
2026-03-28 20:12:30 +0400

    Сделано задание 1

---
+++ src/main/java/ru/otus/java/basic/second_test.java
@@ -0,0 +1,58 @@
+package ru.otus.java.basic;
+
+public class second_test {
+    public static void main(String[] args) {
+        greetings();
+        checkSign(1, 23, -35);
+        selectColor();
+        compareNumbers();
+        addOrSubtractAndPrint(2, 5, false);
+    }
+
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        int sum = a + b + c;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void selectColor() {
+        int data = 15;
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data > 10 && data <= 20) {
+            System.out.println("Жултый");
+        } else if (data > 20) {
+            System.out.println("Зеленыйй");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 2;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+        if (increment == true) {
+            System.out.println(initValue += delta);
+        } else {
+            System.out.println(initValue -= delta);
+        }
+    }
+
+}
